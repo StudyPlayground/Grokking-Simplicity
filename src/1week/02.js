4a9c0b9eef48b4a91668bfc1b4e77fbe8ac799f7
ohtmm <cwd05011@gmail.com>
2023-11-28 12:17:21 +0900

    1week : 02.js 명시적 입력을 통해 전달받은 인자라도 변경 가능한 배열일 경우 복사하도록 수정

--- src/1week/02.js
+++ src/1week/02.js
@@ -1,8 +1,9 @@
 function accumulate(arr) {
+  const copyArr = arr.slice();
   let accumulator = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    accumulator += arr[i];
+  for (let i = 0; i < copyArr.length; i++) {
+    accumulator += copyArr[i];
   }
 
   return accumulator;
