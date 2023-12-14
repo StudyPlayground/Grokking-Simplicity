76cdd14a897ff4022722a079580eea62ca1744c7
ohtmm <cwd05011@gmail.com>
2023-12-14 17:31:31 +0900

    2week: 1차 리팩토링

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,22 +1,28 @@
+
+
 var shopping_cart = [];
 var shopping_cart_total = 0;
 
-// cart layer
 const cartLayer = document.getElementById('cart-layer');
 const cartList = document.getElementById('cart-list');
 const cartIcon = document.querySelector('.carts>.icons');
+const productList = document.querySelector('.items');
 
-function open_cart() {
-  cartLayer.style.display = 'block';
-}
 
-function close_cart() {
-  cartLayer.style.display = 'none';
-}
+// event_cart :  oepn & close
+cartIcon.addEventListener('click', () => {
+  show_dom(cartLayer);
+  set_cart_list_dom(shopping_cart, cartList);
+});
 
-function show_cart_list() {
-  cartList.innerHTML = shopping_cart
-    .map(
+cartLayer.addEventListener('click', (e) => {
+  if (e.target !== cartLayer) return;
+  hide_dom(cartLayer);
+});
+
+
+function set_cart_list_dom(cartItems, cartContainer) {
+  cartContainer.innerHTML = cartItems.map(
       (item) => `
         <div class='cart-item'>
         <span class='category'>${item.category}</span>
@@ -28,56 +34,58 @@ function show_cart_list() {
     .join('');
 }
 
-cartIcon.addEventListener('click', () => {
-  open_cart();
-  show_cart_list();
-});
 
-cartLayer.addEventListener('click', (e) => {
-  if (e.target !== cartLayer) return;
-  close_cart();
-});
-
-function formatCurrencyToNumber(priceText) {
-  const regex = /,/g;
-  return parseFloat(priceText.replace(regex, ''));
-}
-
-function formatNumberToCurrency(priceNum) {
-  return priceNum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원';
-}
-
-// items
-document.querySelectorAll('button').forEach((button) =>
+// event_products - add to cart   
+productList.querySelectorAll('.add-to-cart').forEach((button) =>
   button.addEventListener('click', ({ target }) => {
     const name = target.parentNode.querySelector('.menu-name').textContent;
     const category = target.parentNode.querySelector('.category').textContent;
-    const price = formatCurrencyToNumber(
-      target.parentNode.querySelector('.price').textContent
-    );
+    const price = formatCurrencyToNumber(target.parentNode.querySelector('.price').textContent);
 
-    add_item_to_cart({ name, category, price });
+    /* 전역 변수를 인자로 넘겨줬지만, 공유 객체이므로 카피해서 사용해야 하겠지? */
+
+    // cart change
+    shopping_cart = add_item_to_cart({ name, category, price }, shopping_cart);
+
+    // total change
+    shopping_cart_total = calc_cart_total(shopping_cart)
+
+    // product change
+    update_shipping_icons(shopping_cart_total);
   })
 );
 
-function add_item_to_cart(item) {
-  shopping_cart.push(item);
-  calc_cart_total();
+function add_item_to_cart(item, cart) {
+  return [...cart, item]
 }
 
-// Q : 왜 cart_total 과 tax DOM 업데이트를 2번 해줘야 할까?
-function calc_cart_total() {
-  shopping_cart_total = 0;
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
+// header(total), main(product), cart(product)
+function calc_cart_total(cart) {
+  const total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    total += item.price;
   }
-  set_total_price_dom(shopping_cart_total);
-  update_tax_dom(shopping_cart_total);
-  update_shipping_icons();
+  // Q : 왜 cart_total 과 tax DOM 업데이트를 2번 해줘야 할까?
+  set_total_price_dom(total);
+  update_tax_dom(total);
+  return total;
 }
 
-function update_shipping_icons() {
+//header(total)
+function update_tax_dom(total) {
+  // tax를 더한다는 것을 제외하면 set_calc_total_dom 동작과 같음, calc+total 을 관리하는 돔으로 통합 
+  set_total_price_dom(total + calc_tax(total));
+}
+
+function set_total_price_dom(totalNum) {
+  document.querySelector('.total-price').textContent =
+    formatNumberToCurrency(totalNum);
+}
+
+
+// main(product)
+function update_shipping_icons(total) {
   var buy_buttons = get_buy_buttons_dom();
   for (var i = 0; i < buy_buttons.length; i++) {
     // 제품 item + shpping icon 기능 추가된 객체
@@ -85,16 +93,17 @@ function update_shipping_icons() {
     const btn_item_price = formatCurrencyToNumber(
       btn.parentNode.querySelector('.price').textContent
     );
-    if (btn_item_price + shopping_cart_total >= 20000)
+    if (is_free_shipping(btn_item_price, total))
       btn.show_free_shipping_icon();
     else btn.hide_free_shipping_icon();
   }
 }
 
-// Q: 구매하기 버튼,
+// main(product)
 function get_buy_buttons_dom() {
   var buttons = [];
   const add_to_cart_btns = document.querySelectorAll('.add-to-cart');
+  // Q: 구매하기 버튼
   // shopping_cart를 도는 게 아니라 상품 전체를 돌아야 함
   for (var i = 0; i < add_to_cart_btns.length; i++) {
     var btn = add_to_cart_btns[i];
@@ -114,16 +123,9 @@ function get_buy_buttons_dom() {
   return buttons;
 }
 
-// tax를 더한다는 것을 제외하면 set_calc_total_dom 동작과 같음, calc+total 을 관리하는 돔으로 통합하는 건 ?
-function update_tax_dom(cartTotal) {
-  const tax = calc_tax(cartTotal);
-  set_total_price_dom(cartTotal + tax);
-}
 
-function set_total_price_dom(totalNum) {
-  document.querySelector('.total-price').textContent =
-    formatNumberToCurrency(totalNum);
-}
+
+
 
 // function set_tax_dom(value) {
 //   document.querySelector('.total-price').textContent = formatNumberToCurrency(value);
@@ -133,6 +135,34 @@ function set_total_price_dom(totalNum) {
 //   document.querySelector('.total-price').textContent = formatNumberToCurrency(total);
 // }
 
+
+
+// 비즈니스 - 세금 
 function calc_tax(total) {
   return total * 0.1;
 }
+// 비즈니스 - 배송 
+function is_free_shipping( itemPrice, cartTotal){
+  return itemPrice + cartTotal >= 20000
+}
+
+// utils ? - DOM
+function show_dom(el) {
+  el.style.display = 'block';
+}
+
+// utils ? - DOM
+function hide_dom() {
+  el.style.display = 'none';
+}
+
+// utils - format
+function formatCurrencyToNumber(priceText) {
+  const regex = /,/g;
+  return parseFloat(priceText.replace(regex, ''));
+}
+
+function formatNumberToCurrency(priceNum) {
+  return priceNum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원';
+}
+
