7da139d956f873e977540bc65322ef965aa5522a
jiyoung <cwd05011@gmail.com>
2023-12-18 20:17:00 +0900

    week2: 2-2차 리팩토링

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,13 +1,14 @@
-import ShoppingCart from "./cart.js";
+import ShoppingCart from './cart.js';
 
-
-const {cartGetter: getCart, cartTotalGetter: getCartTotal, cartSetter: setCart} = ShoppingCart()
-
+const {
+  cartGetter: getCart,
+  cartTotalGetter: getCartTotal,
+  cartSetter: setCart,
+} = ShoppingCart();
 
 const cartLayer = document.getElementById('cart-layer');
 const cartIcon = document.querySelector('.carts>.icons');
 const productList = document.querySelector('.items');
-
 
 cartIcon.addEventListener('click', () => {
   show_dom(cartLayer);
@@ -19,14 +20,73 @@ cartLayer.addEventListener('click', (e) => {
   hide_dom(cartLayer);
 });
 
+productList.querySelectorAll('.add-to-cart').forEach((button) =>
+  button.addEventListener('click', ({ target }) => {
+    setCart('add', makeCartItem(target.parentNode));
+    update_shipping_icons_dom(getCartTotal());
+    set_total_price_dom(getCartTotal());
+  })
+);
 
+// action - dom : get_dom_value 호출
+function makeCartItem(targetProduct = document) {
+  const PRODUCT_KEYS = [
+    {
+      type: 'string',
+      className: 'category',
+    },
+    {
+      type: 'number',
+      className: 'price',
+    },
+    {
+      type: 'string',
+      className: 'menu-name',
+    },
+  ];
+
+  const cartItem = {};
+  PRODUCT_KEYS.forEach((key) => {
+    cartItem[key.className] = get_dom_value(
+      key.className,
+      key.type,
+      targetProduct
+    );
+  });
+  return cartItem;
+}
+
+// action - dom : get_buy_buttons_dom 호출
+function update_shipping_icons_dom(total) {
+  const buy_buttons = get_buy_buttons_dom();
+  buy_buttons.forEach((btn) => {
+    const btn_item_price = get_dom_value('price', 'number', btn.parentNode);
+    if (is_free_shipping(btn_item_price, total)) btn.show_free_shipping_icon();
+    else btn.hide_free_shipping_icon();
+  });
+}
+
+// action - dom [dynamicSelector]
+function get_dom_value(className, valueType, target = document) {
+  const el = target.querySelector(`.${className}`);
+  if (!el) return null;
+
+  if (valueType === 'string') {
+    return el.textContent;
+  } else {
+    return formatToNumber(el.textContent);
+  }
+}
+
+// action - dom 'cart-list'
 function set_cart_list_dom() {
   const cartList = document.getElementById('cart-list');
-  cartList.innerHTML = getCart().map(
+  cartList.innerHTML = getCart()
+    .map(
       (item) => `
         <div class='cart-item'>
         <span class='category'>${item.category}</span>
-          <h3 class='name'>${item.name}</h3>
+          <h3 class='name'>${item['menu-name']}</h3>
           <p class='price'>${formatNumberToCurrency(item.price)}</p>
         </div>
         `
@@ -34,82 +94,40 @@ function set_cart_list_dom() {
     .join('');
 }
 
-function get_dom_text (className, target = document){
-return target.querySelector(`${className}`).textContent;
-}
-
-function get_dom_num (className, target = document){
-  return formatToNumber(target.querySelector(`${className}`).textContent);
-}
-
-const ITEM_STRING =['name', 'category'];
-const ITEM_NUMBER = ['price']
-
-function make_cart_item(keys,target = document){
-  const cartItem = {}
-  keys.forEach(key => {
-    if( ITEM_STRING.includes(key)){
-      cartItem[key] = get_dom_text(key,target)
-    }else{
-      cartItem[key] = get_dom_num(key, target)
-    }
-  })
-  return cartItem;
-}
-
-
-productList.querySelectorAll('.add-to-cart').forEach((button) =>
-  button.addEventListener('click', ({ target }) => {
-    const name = get_dom_text('name', target.parentNode);
-    const category = get_dom_text('category', target.parentNode);
-    const price = get_dom_num('price', target.parentNode);
-
-    setCart('add', {name, category, price})
-    update_shipping_icons_dom(getCartTotal());
-    set_total_price_dom(getCartTotal());
-    })
-);
-
+// action - dom 'total-price'
 function set_total_price_dom(total) {
-  document.querySelector('.total-price').textContent =
-    formatNumberToCurrency(total+calc_tax(total));
+  document.querySelector('.total-price').textContent = formatNumberToCurrency(
+    total + calc_tax(total)
+  );
 }
 
-function update_shipping_icons_dom(total) {
-  const buy_buttons = get_buy_buttons_dom();
-  buy_buttons.forEach((btn)=>{
-    const btn_item_price = get_dom_num('price',btn.parentNode)
-    if (is_free_shipping(btn_item_price, total))
-      btn.show_free_shipping_icon();
-    else btn.hide_free_shipping_icon();
-  }
-  ) 
-}
-
+// action - dom 'add-to-cart'
 function get_buy_buttons_dom() {
-  const add_to_cart_btns = Array.from(document.querySelectorAll('.add-to-cart'));
-  return add_to_cart_btns.map(btn => {
+  const add_to_cart_btns = Array.from(
+    document.querySelectorAll('.add-to-cart')
+  );
+  return add_to_cart_btns.map((btn) => {
     btn.show_free_shipping_icon = function () {
       this.parentNode
         .querySelector('.free-shipping-icon')
         .classList.remove('hidden');
-      };
+    };
     btn.hide_free_shipping_icon = function () {
       this.parentNode
         .querySelector('.free-shipping-icon')
         .classList.add('hidden');
-      };
-      return btn
-    })}
+    };
+    return btn;
+  });
+}
 
-
-// 비즈니스 - 세금 
+// 비즈니스 - 세금
 function calc_tax(total) {
   return total * 0.1;
 }
-// 비즈니스 - 배송 
-function is_free_shipping( itemPrice, cartTotal){
-  return itemPrice + cartTotal >= 20000
+// 비즈니스 - 배송
+function is_free_shipping(itemPrice, cartTotal) {
+  return itemPrice + cartTotal >= 20000;
 }
 
 // utils ? - DOM
@@ -123,18 +141,11 @@ function hide_dom(el) {
 }
 
 // utils - format
-function formatToNumber(text){
-  return parseFloat(text.replace(/[^\d]/g,''));
-}
-/*
-function formatCurrencyToNumber(priceText) {
-  const regex = /,/g;
-  return parseFloat(priceText.replace(regex, ''));
+function formatToNumber(text) {
+  return parseFloat(text.replace(/[^\d]/g, ''));
 }
 
-*/
-
+// utils - format
 function formatNumberToCurrency(priceNum) {
   return priceNum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원';
 }
-
--- src/2week/cart.js
+++ src/2week/cart.js
@@ -1,33 +1,30 @@
 const ShoppingCart = () => {
-    let shopping_cart = [];
+  let shopping_cart = [];
 
+  const cartGetter = () => [...shopping_cart];
 
-    const cartGetter = () => [...shopping_cart]
-    
-    const cartTotalGetter = () =>  shopping_cart.reduce((acc,item)=> acc + item.price, 0);
-    
-    const cartSetter = (action, item) => {
-        switch(action){
-            case 'add' :
-                shopping_cart = [...shopping_cart, item]
-                console.log(shopping_cart)
-                break;
-            case 'update' :
-                
-               break;
-            case 'delete':
+  const cartTotalGetter = () =>
+    shopping_cart.reduce((acc, item) => acc + item.price, 0);
 
-                break; 
-            default :
-                throw new Error('Action must be one of "add", "update", or "delete"')
-            }
-        }
+  const cartSetter = (action, item) => {
+    switch (action) {
+      case 'add':
+        shopping_cart = [...shopping_cart, item];
+        break;
+      case 'update':
+        break;
+      case 'delete':
+        break;
+      default:
+        throw new Error('Action must be one of "add", "update", or "delete"');
+    }
+  };
 
+  return {
+    cartGetter,
+    cartTotalGetter,
+    cartSetter,
+  };
+};
 
-    return {
-        cartGetter,
-        cartTotalGetter,
-        cartSetter,
-    }}
-
-    export default ShoppingCart;
+export default ShoppingCart;
