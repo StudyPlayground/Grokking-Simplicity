73777a51cc5d7e22ecf497d4025ee808b949eaa5
wogha95 <wogha95@naver.com>
2023-12-24 18:19:02 +0900

    추상화 계층 : 계층 수 줄이기, 화살표 길이 줄이기

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,73 +1,53 @@
 var shopping_cart = [];
 var shopping_cart_total = 0;
+const TAX = 0.1;
 
-//NOTE 추상화 벽 외부 3층
+//NOTE 추상화 벽 외부 2층
 document.querySelectorAll('button').forEach(button =>
   button.addEventListener('click', ({ target }) => {
     const name = getElementTextContent(target.parentNode, '.menu-name');
     const category = getElementTextContent(target.parentNode, '.category');
     const price = getElementTextContent(target.parentNode, '.price');
 
-    add_item_to_cart(shopping_cart, { name, category, price });
-    shopping_cart_total = calc_cart_total(shopping_cart);
+    shopping_cart = add_item_to_cart(shopping_cart, { name, category, price });
+    shopping_cart_total = getTotalPrice(shopping_cart);
     set_cart_total_dom(shopping_cart_total);
-    update_shipping_icons(shopping_cart, shopping_cart_total);
-    update_tax_dom(shopping_cart_total);
+    update_shipping_icons(get_buy_buttons_dom(shopping_cart), shopping_cart_total);
+    set_tax_dom(calculatedTotalByTax(shopping_cart_total, TAX));
+    console.log(shopping_cart)
   }),
 );
 
-
-//NOTE 추상화 벽 외부 2층
+//NOTE 추상화 벽 외부 1층
 function add_item_to_cart(cart, item) {
-  cart.push(item);
-  console.log(cart);
-}
-
-function calc_cart_total(cart) {
-  let result = 0;
-  for (var i = 0; i < cart.length; i++) {
-    var item = cart[i];
-    result += getPriceValue(item.price);
-  }
-  return result;
+  return addItem(cart, generateItem(item));
 }
 
 function set_cart_total_dom(total) {
   setElementTextContent(document, '.total-price', total);
 }
 
-function update_shipping_icons(cart, total) {
-  var buy_buttons = get_buy_buttons_dom(cart);
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var item = buy_buttons[i];
-    console.log(item);
-    if (getPriceValue(item.price) + total >= 20000) item.show_free_shopping_icon();
-    else item.hide_free_shopping_icon();
-  }
+function update_shipping_icons(buy_buttons, total) {
+  doSomethingWithItem(buy_buttons, (item) => {
+    if (getPriceNumber(getValue(item, 'price')) + total >= 20000){
+      item.show_free_shopping_icon();
+    }
+    else {
+      item.hide_free_shopping_icon();
+    }
+  })
 }
 
-function update_tax_dom(total) {
-  const TAX = 0.1;
-  set_tax_dom(total * (1 - TAX));
-}
-
-
-//NOTE 추상화 벽 외부 1층
 function get_buy_buttons_dom(cart) {
-  var buttons = [];
-
-  for (var i = 0; i < cart.length; i++) {
-    var item = cart[i];
+  return doSomethingWithItem(cart, (item) => {
     item.show_free_shopping_icon = function () {
-      console.log('DOM 의 아이콘을 보여줍니다');
+      console.log(`${getValue(item, 'name')} / ${getValue(item, 'price')} / DOM 의 아이콘을 보여줍니다`);
     };
     item.hide_free_shopping_icon = function () {
-      console.log('DOM 의 아이콘을 숨깁니다');
+      console.log(`${getValue(item, 'name')} / ${getValue(item, 'price')} / DOM 의 아이콘을 숨깁니다`);
     };
-    buttons.push(item);
-  }
-
-  return buttons;
+    return item;
+  })
 }
 
 function set_tax_dom(value) {
@@ -77,7 +57,7 @@ function set_tax_dom(value) {
 
 //NOTE 추상화 벽
 function getTotalPrice(cart) {
-  return getTotalByKey(cart, 'price');
+  return cart.reduce((total, item) => total + getPriceNumber(getValue(item, 'price')), 0);
 }
 
 function getElementTextContent(rootNode, selector) {
@@ -88,21 +68,28 @@ function setElementTextContent(rootNode, selector, newText) {
   rootNode.querySelector(selector).textContent = newText;
 }
 
-function getPriceValue(price) {
+function getPriceNumber(price) {
   return Number(price.replaceAll('원', '').replaceAll(',', ''))
 }
 
-//NOTE 추상화 벽 내부
+function calculatedTotalByTax(total, tax) {
+  return total * (1 - tax);
+}
+
+function generateItem({name, category, price}) {
+  return {name, category, price}
+}
+
 function getValue(item, key) {
   return item[key];
 }
 
-function addItemInArray(array, item) {
+function addItem(array, item) {
   //TODO 불변성 필요
-  return array.push(item);
+  array.push(item);
+  return array;
 }
 
-function getTotalByKey(array, key) {
-  return array.reduce((total, item) => total + getValue(item, key), 0);
+function doSomethingWithItem(cart, f) {
+  return cart.map(f);
 }
-
